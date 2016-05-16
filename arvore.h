7ffe93cc2b50166398a20a979ab4b246cc0da59c
Arthur Gonçalves <arthur_lima_1@hotmail.com>
2016-05-16 02:14:40 -0300

    Adicionada função de adicionar cliente

--- arvore.h
+++ arvore.h
@@ -1,6 +1,6 @@
 typedef struct cliente {
     int cpf;
-    char nome[20], telefeone[16], rg[10];
+    char nome[30], telefone[16], rg[10];
 } Cliente;
 
 typedef struct noArvore {
@@ -18,15 +18,40 @@ NoArvore criaArvore(){
 	return novaArvore;
 }
 
-void inserirNoArvore(NoArvore* raiz, Cliente* c){
-	
+void inserirNoArvore(NoArvore* arvore, Cliente* c){
+	if(arvore == NULL){
+		arvore = (NoArvore*) malloc (sizeof(NoArvore));
+		arvore->conteudo = c;
+		arvore->contas = NULL;
+		arvore->esquerda = NULL;
+		arvore->direita = NULL;
+		return;
+	}
+	if(arvore->conteudo->cpf < c->cpf){
+		inserirNoArvore(arvore->direita, c);
+		return;
+	}
+	if(arvore->conteudo->cpf > c->cpf)
+		inserirNoArvore(arvore->esquerda, c);
+	else
+		printf("Já existe um cliente cadastrado com esse cpf.\n");
 }
 
-void excluirNoArvore(NoArvore* raiz, Cliente* c){
+void excluirNoArvore(NoArvore* arvore, Cliente* c){
 
 }
 
-Cliente encontrarPorCPF(NoArvore* raiz, int cpf){
+void novoCliente(NoArvore* arvore, int cpf, char nome[30], char tel[16], char rg[10]){
+	Cliente *c;
+	c = (Cliente*) malloc (sizeof(Cliente));
+	c->cpf = cpf;
+	strcpy(c->nome, nome);
+	strcpy(c->telefone, tel);
+	strcpy(c->rg, rg);
+	inserirNoArvore(arvore, c);
+}
+
+Cliente encontrarPorCPF(NoArvore* arvore, int cpf){
 
 }
 
@@ -34,6 +59,6 @@ void exibirCliente(Cliente c){
 
 }
 
-void alterarDadosCliente(int cpf, char nome[20], char tel[16]){
+void alterarDadosCliente(int cpf, char nome[30], char tel[16]){
 
 }

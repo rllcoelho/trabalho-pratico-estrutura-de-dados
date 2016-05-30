e82ce618e322c27d71cfa197203c065f263f133d
Arthur Gonçalves <arthur_lima_1@hotmail.com>
2016-05-30 03:22:12 -0300

    Menu de cliente completamente pronto, todas funções de árvore prontas

--- arvore.h
+++ arvore.h
@@ -10,37 +10,34 @@ typedef struct noArvore {
     struct noArvore *esquerda, *direita;
 } NoArvore;
 
-NoArvore criaArvore(){
-	NoArvore novaArvore;
-	novaArvore.conteudo = NULL;
-	novaArvore.contas = NULL;
-	novaArvore.esquerda = NULL;
-	novaArvore.direita = NULL;
+NoArvore* criaArvore(){
+	NoArvore *novaArvore;
+	novaArvore = (NoArvore*) malloc (sizeof(NoArvore));
+	novaArvore->conteudo = NULL;
+	novaArvore->contas = NULL;
+	novaArvore->esquerda = NULL;
+	novaArvore->direita = NULL;
 	return novaArvore;
 }
 
-void inserirNoArvore(NoArvore* arvore, Cliente* c){
-	printf("entrou\n");
-	if(!arvore || !arvore->conteudo){
-		arvore = (NoArvore*) malloc (sizeof(NoArvore));
-		arvore->conteudo = c;
-		printf("%d\n",arvore->conteudo->cpf);
-		arvore->contas = NULL;
-		arvore->esquerda = NULL;
-		arvore->direita = NULL;
+void inserirNoArvore(NoArvore** arvore, Cliente* c){
+	if(!(*arvore) || !(*arvore)->conteudo){
+		*arvore = criaArvore();
+		(*arvore)->conteudo = c;
+		printf("Cliente adicionado com sucesso!\n\n");
 		return;
 	}
-	if(arvore->conteudo->cpf < c->cpf){
-		inserirNoArvore(arvore->direita, c);
+	if((*arvore)->conteudo->cpf < c->cpf){
+		inserirNoArvore(&(*arvore)->direita, c);
 		return;
 	}
-	if(arvore->conteudo->cpf > c->cpf)
-		inserirNoArvore(arvore->esquerda, c);
+	if((*arvore)->conteudo->cpf > c->cpf)
+		inserirNoArvore(&(*arvore)->esquerda, c);
 	else
-		printf("Já existe um cliente cadastrado com esse cpf.\n");
+		printf("Já existe um cliente cadastrado com esse cpf.\n\n\n");
 }
 
-void novoCliente(NoArvore* arvore, int cpf, char nome[30], char tel[16], char rg[10]){
+void novoCliente(NoArvore** arvore, int cpf, char nome[30], char tel[16], char rg[10]){
 	Cliente *c;
 	c = (Cliente*) malloc (sizeof(Cliente));
 	c->cpf = cpf;
@@ -48,90 +45,104 @@ void novoCliente(NoArvore* arvore, int cpf, char nome[30], char tel[16], char rg
 	strcpy(c->telefone, tel);
 	strcpy(c->rg, rg);
 	inserirNoArvore(arvore, c);
-	printf("%d\n",arvore->conteudo->cpf);
 }
 
-Cliente* encontrarPorCPF(NoArvore* arvore, int cpf){
-	printf("aaaaaaa\n");
-	if(!arvore){
-		return 0;
+void encontrarPorCPF(NoArvore* arvore, Cliente** c){
+	if(!arvore || !arvore->conteudo){
+		(*c)->cpf = 0;
+		return;
 	}
-	if(arvore->conteudo->cpf < cpf){
-		printf("%d\n",arvore->conteudo->cpf);
-		encontrarPorCPF(arvore->direita, cpf);
-		return 0;
+	if(arvore->conteudo->cpf < (*c)->cpf){
+		encontrarPorCPF(arvore->direita, c);
+		return;
 	}
-	if(arvore->conteudo->cpf > cpf)
-		encontrarPorCPF(arvore->esquerda, cpf);
+	if(arvore->conteudo->cpf > (*c)->cpf)
+		encontrarPorCPF(arvore->esquerda, c);
 	else
-		return arvore->conteudo;
-	printf("aaaaaaa\n");
-	return 0;
+		*c = arvore->conteudo;
 }
 
-void exibirCliente(NoArvore arvore, int cpf){
+void exibirCliente(NoArvore* arvore, int cpf){
 	Cliente *c;
-	c = encontrarPorCPF(&arvore, cpf);
-	printf("%d\n",arvore.conteudo->cpf);
-	if (c){
+	c = (Cliente*) malloc (sizeof(Cliente));
+	c->cpf = cpf;
+	encontrarPorCPF(arvore, &c);
+	if (c->cpf){
 		printf("Dados do cliente:\n\n");
 		printf("Nome: %s\n", c->nome);
 		printf("Telefone: %s\n", c->telefone);
-		printf("RG: %s\n", c->rg);
+		printf("RG: %s\n\n\n", c->rg);
 	}else
-		printf("Não há cliente com esse CPF no sistema.\n");	
+		printf("Não há cliente com esse CPF no sistema.\n\n\n");	
 }
 
-void alterarDadosCliente(NoArvore arvore, int cpf, char nome[30], char tel[16], char rg[10]){
+void alterarDadosCliente(NoArvore** arvore, int cpf, char nome[30], char tel[16], char rg[10]){
 	Cliente *c;
-	c = encontrarPorCPF(&arvore, cpf);
-	strcpy(c->nome, nome);
-	strcpy(c->telefone, tel);
-	strcpy(c->rg, rg);
-	printf("Cliente editado com sucesso!");
+	c = (Cliente*) malloc (sizeof(Cliente));
+	c->cpf = cpf;
+	encontrarPorCPF(*arvore, &c);
+	if (c->cpf){
+		strcpy(c->nome, nome);
+		strcpy(c->telefone, tel);
+		strcpy(c->rg, rg);
+		printf("Cliente editado com sucesso!\n\n\n");
+	} else
+		printf("Não há cliente com esse CPF no sistema.\n\n\n");
 }
 
-void antecessor(NoArvore arvore, NoArvore *no){
-	if(no->direita){
-        antecessor(arvore, no->direita);
+void antecessor(NoArvore* arvore, NoArvore** no){
+	if((*no)->direita){
+        antecessor(arvore, &(*no)->direita);
         return;
     }
-    arvore.conteudo = no->conteudo;
-    arvore.contas = no->contas;
+    arvore->conteudo = (*no)->conteudo;
+    arvore->contas = (*no)->contas;
     arvore = *no;
-    no = no->esquerda;
-    free(&arvore);
+    *no = (*no)->esquerda;
+    free(arvore);
 }
 
-void excluirNoArvore(NoArvore *arvore, Cliente* c){
+void excluirNoArvore(NoArvore** arvore, Cliente* c){
 	NoArvore* aux;
-	if(!arvore){
-		printf("Não há cliente com esse cpf no sistema.");
+	if(!(*arvore)){
+		printf("Não há cliente com esse cpf no sistema.\n\n\n");
 		return;
 	}
-	if(c->cpf < arvore->conteudo->cpf){
-        excluirNoArvore(arvore->esquerda, c);
+	if(c->cpf < (*arvore)->conteudo->cpf){
+        excluirNoArvore(&(*arvore)->esquerda, c);
         return;
 	}
-    if(c->cpf > arvore->conteudo->cpf){
-        excluirNoArvore(arvore->direita, c);
+    if(c->cpf > (*arvore)->conteudo->cpf){
+        excluirNoArvore(&(*arvore)->direita, c);
         return;
 	}
-    if(arvore->direita == NULL){
-        aux = arvore;
-        arvore = arvore->esquerda;
+    if(!(*arvore)->direita){
+        aux = *arvore;
+        *arvore = (*arvore)->esquerda;
         //excluirListaDupla(aux->contas); //mudar nome para função do arquivo de listas duplas
         free(aux->conteudo);
         free(aux);
         return;
     }
-    if(arvore->esquerda){
-        antecessor(*arvore,arvore->esquerda);
+    if((*arvore)->esquerda){
+        antecessor(*arvore, &(*arvore)->esquerda);
         return;
     }
-    aux = arvore;
-    arvore = arvore->direita;
+    aux = *arvore;
+    *arvore = (*arvore)->direita;
     //excluirListaDupla(aux->contas); //mudar nome para função do arquivo de listas duplas
     free(aux->conteudo);
     free(aux);
+}
+
+void removerCliente(NoArvore** arvore, int cpf){
+	Cliente *c;
+	c = (Cliente*) malloc (sizeof(Cliente));
+	c->cpf = cpf;
+	encontrarPorCPF(*arvore, &c);
+	if (c->cpf){
+		excluirNoArvore(arvore, c);
+		printf("Cliente removido com sucesso!\n\n\n");
+	}else
+		printf("Não há cliente com esse CPF no sistema.\n\n\n");	
 }
--- main.c
+++ main.c
@@ -3,58 +3,81 @@
 #include <string.h>
 #include "arvore.h"
 
-void menuCliente(NoArvore* arvore){
+void menuCliente(NoArvore** arvore){
 	printf("Selecione a operação desejada:\n");
 	printf("Inserir um cliente: digite 1\n");
 	printf("Editar os dados de um cliente: digite 2\n");
-	printf("Remover um cliente: digite 3\n");
-	printf("Exibir um cliente: digite 4\n");
-	printf("Voltar ao menu principal: digite 5\n");
-	int opcao, cpf;
+	printf("Exibir um cliente: digite 3\n");
+	printf("Remover um cliente: digite 4\n");
+	printf("Voltar ao menu principal: digite 5\n\n");
+	int opcao, cpf, confirma;
 	scanf("%d",&opcao);
+	printf("\n");
 	switch(opcao){
 		case 1: ;
 			char nome[30],tel[16],rg[10];
-			printf("Digite os dados do novo cliente\n");
+			printf("Digite os dados do novo cliente:\n");
 			printf("CPF: ");
 			scanf("%d", &cpf);
 			printf("Nome: ");
 			scanf("%s", nome);
-			//fgets(nome, sizeof(nome), stdin);
 			printf("Telefone: ");
 			scanf("%s", tel);
-			//fgets(tel, sizeof(tel), stdin);
 			printf("RG: ");
 			scanf("%s", rg);
-			//fgets(rg, sizeof(rg), stdin);
+			printf("\n");
 			novoCliente(arvore, cpf, nome, tel, rg);
 			break;
 		case 2:
+			printf("Digite o CPF do cliente: ");
+			scanf("%d",&cpf);
+			printf("Tem certeza de que deseja editar os dados do cliente com CPF %d? (1 para sim, 2 para não)\n", cpf);
+			scanf("%d", &confirma);
+			if (confirma == 1){
+				printf("\n");
+				printf("Digite abaixo os novos valores que deseja para os dados do cliente:\n");
+				printf("Nome: ");
+				scanf("%s", nome);
+				printf("Telefone: ");
+				scanf("%s", tel);
+				printf("RG: ");
+				scanf("%s", rg);
+				alterarDadosCliente(arvore, cpf, nome, tel, rg);
+			}
 			break;
 		case 3:
-			break;
-		case 4:
-			printf("Digite o CPF do cliente\n");
+			printf("Digite o CPF do cliente: ");
 			scanf("%d",&cpf);
 			exibirCliente(*arvore,cpf);
 			break;
+		case 4:
+			printf("Digite o CPF do cliente: ");
+			scanf("%d",&cpf);
+			printf("Tem certeza de que deseja remover do sistema o cliente com CPF %d? (1 para sim, 2 para não)\n", cpf);
+			scanf("%d", &confirma);
+			if (confirma == 1){
+				printf("\n");
+				removerCliente(arvore, cpf);
+			}
+			break;
 		case 5:
-			exit(0);
 			break;
 		default:
-			printf("Opção inválida, tente novamente");
+			printf("Opção inválida, tente novamente\n\n\n");
+			menuCliente(arvore);
 			break;
 	}
 }
 
-void menuPrincipal(NoArvore* arvore){
+void menuPrincipal(NoArvore** arvore){
 	printf("Escolha uma das opções a seguir para continuar:\n");
 	printf("Operações sobre um cliente: digite 1\n");
 	printf("Operações sobre as contas de um cliente: digite 2\n");
 	printf("Operações sobre uma conta específica de um cliente: digite 3\n");
-	printf("Encerrar programa: digite 4\n");
+	printf("Encerrar programa: digite 4\n\n");
 	int opcao;
 	scanf("%d",&opcao);
+	printf("\n");
 	switch(opcao){
 		case 1:
 			menuCliente(arvore);
@@ -69,24 +92,15 @@ void menuPrincipal(NoArvore* arvore){
 			exit(0);
 			break;
 		default:
-			printf("Opção inválida, tente novamente\n");
+			printf("Opção inválida, tente novamente\n\n\n");
 			break;
 	}
 	menuPrincipal(arvore);
 }
 
 int main(){
-	NoArvore arvore;
+	NoArvore* arvore;
 	arvore = criaArvore();
-	/*int cpf;
-	char nome[30],tel[16],rg[10];
-	printf("AAAAAAAAAAA ");
-	cpf = 12345;
-	strcpy(nome, "pafuncio");
-	strcpy(tel, "99999-9999");
-	strcpy(rg, "999.999");
-	printf("AAAAAAAAAAA ");
-	novoCliente(&arvore, cpf, nome, tel, rg);*/
 	menuPrincipal(&arvore);
 	return 0;
 }
